ff900cff53bf44d55730b183f8d8203a7bc1481a
Mark Van der Merwe <markvdm@umich.edu>
2025-03-24 17:23:03 -0400

    More varied sampling of poses

--- OpticalSimulation/generate_sim_depth.py
+++ OpticalSimulation/generate_sim_depth.py
@@ -2,6 +2,7 @@ import argparse
 import copy
 import multiprocessing
 import os
+import time
 
 import cv2
 
@@ -29,23 +30,52 @@ def get_sensor_points(imgw: int = 320, imgh: int = 240, mmpp: float = 0.065):
     return points
 
 
+def compute_depth(obj_mesh: trimesh.Trimesh, sensor_points: np.ndarray):
+    ray_mesh_intersector = trimesh.ray.ray_pyembree.RayMeshIntersector(obj_mesh)
+    min_points = copy.deepcopy(sensor_points)
+    min_points[:, 1] = -0.1
+    intersect_points, ray_indices, _ = ray_mesh_intersector.intersects_location(
+        min_points, np.array([[0, 1, 0]] * min_points.shape[0]))
+    intersect_mask = intersect_points[:, 1] < 0
+    intersect_points[~intersect_mask, 1] = 0.0
+    sensor_points[ray_indices] = intersect_points
+    return sensor_points, intersect_mask
+
+
 class GenerateSimDepth:
 
-    def __init__(self, obj_cfg: dict, out_dir: str, num: int, length: int):
+    def __init__(self, obj_cfg: dict, out_dir: str, num: int, length: int, seed: int):
         self.obj_cfg = obj_cfg
         self.out_dir = out_dir
         self.num = num
         self.length = length
+        self.seed = seed
+
+        self.imgw = 320
+        self.imgh = 240
+        self.mmpp = 0.065
+        self.sensor_x_min = -(self.imgw / 2) * self.mmpp / 1000.0
+        self.sensor_x_max = (self.imgw / 2) * self.mmpp / 1000.0
+        self.sensor_y_min = -(self.imgh / 2) * self.mmpp / 1000.0
+        self.sensor_y_max = (self.imgh / 2) * self.mmpp / 1000.0
 
     def __call__(self, trial_idx: int):
+        seed_ = self.seed + trial_idx
+        np.random.seed(seed_)
+
         trial_out_dir = os.path.join(self.out_dir, f"trial_{trial_idx}")
         utils.make_dir(trial_out_dir)
+
+        # TODO: Extend pose sampling to support objects with additional rotational components.
 
         max_offset = np.array([0.001, 0.0005, 0.001, 4.0])  # [m, m, m, deg]
         obj_mesh = trimesh.load_mesh(self.obj_cfg["mesh"])
         sample_pose = self.obj_cfg["sample_pose"]
         obj_mesh.apply_transform(np.linalg.inv(tf_helper.BuildMatrixArray(sample_pose)))
         y_offset = np.abs(obj_mesh.bounds[0][1])
+
+        # Get largest offset in our plane of sampling for object pose.
+        obj_offset = np.max(np.abs(obj_mesh.bounds[:, [0, 2]]))
 
         # We will always start with a starting depth of 0.
         # This helps the model reason about no penetration.
@@ -54,15 +84,25 @@ class GenerateSimDepth:
         norm_depth = cv2.normalize(depth_start, None, 0, 255, cv2.NORM_MINMAX)
         cv2.imwrite(os.path.join(trial_out_dir, f"depth_0.png"), norm_depth)
 
-        init_obj_x = np.random.normal(0.0, 0.002)
-        init_obj_z = np.random.normal(0.0, 0.002)
-        init_theta = np.random.uniform(0.0, 2 * np.pi)
-        indent_depth = np.random.uniform(0.0, 0.002)
+        while True:
+            init_obj_x = np.random.uniform(self.sensor_x_min - obj_offset,
+                                           self.sensor_x_max + obj_offset)  # np.random.normal(0.0, 0.002)
+            init_obj_z = np.random.uniform(self.sensor_y_min - obj_offset,
+                                           self.sensor_y_max + obj_offset)  # np.random.normal(0.0, 0.002)
+            init_theta = np.random.uniform(0.0, 2 * np.pi)
+            indent_depth = np.random.uniform(0.0, 0.002)
 
-        grasp_pos = np.array([init_obj_x, y_offset - indent_depth, init_obj_z])
-        grasp_orn = np.array([0.0, init_theta, 0.0])
-        grasp_orn_quat = tr.quaternion_from_euler(*grasp_orn)
-        grasp_pose = np.concatenate([grasp_pos, grasp_orn_quat])
+            grasp_pos = np.array([init_obj_x, y_offset - indent_depth, init_obj_z])
+            grasp_orn = np.array([0.0, init_theta, 0.0])
+            grasp_orn_quat = tr.quaternion_from_euler(*grasp_orn)
+            grasp_pose = np.concatenate([grasp_pos, grasp_orn_quat])
+
+            # Check that the object is touching the sensor somewhere, o.w. resample.
+            obj_mesh_ = copy.deepcopy(obj_mesh)
+            obj_mesh_.apply_transform(tf_helper.BuildMatrixArray(grasp_pose))
+            _, intersect_mask = compute_depth(obj_mesh_, get_sensor_points())
+            if np.sum(intersect_mask) > 0:
+                break
 
         current_delta = np.zeros(4)
         for step_idx in range(1, self.length + 1):
@@ -82,16 +122,7 @@ class GenerateSimDepth:
             obj_mesh_trial = copy.deepcopy(obj_mesh)
             obj_mesh_trial.apply_transform(tf_helper.BuildMatrixArray(adjust_pose))
 
-            sensor_points = get_sensor_points()
-
-            ray_mesh_intersector = trimesh.ray.ray_pyembree.RayMeshIntersector(obj_mesh_trial)
-            min_points = copy.deepcopy(sensor_points)
-            min_points[:, 1] = -0.1
-            intersect_points, ray_indices, _ = ray_mesh_intersector.intersects_location(
-                min_points, np.array([[0, 1, 0]] * min_points.shape[0]))
-            intersect_mask = intersect_points[:, 1] < 0
-            intersect_points[~intersect_mask, 1] = 0.0
-            sensor_points[ray_indices] = intersect_points
+            sensor_points, _ = compute_depth(obj_mesh_trial, get_sensor_points())
 
             depth = np.abs(sensor_points[:, 1].reshape((240, 320)))
             utils.save_gzip_pickle(depth, os.path.join(trial_out_dir, f"depth_{step_idx}.pkl.gzip"))
@@ -110,7 +141,8 @@ class GenerateSimDepth:
 def generate_sim_depth(obj_cfg: dict, out_dir: str, num: int, length: int, jobs: int = 1):
     utils.make_dir(out_dir)
 
-    gen = GenerateSimDepth(obj_cfg, out_dir, num, length)
+    seed = int(time.time())
+    gen = GenerateSimDepth(obj_cfg, out_dir, num, length, seed)
 
     if jobs == 1:
         for trial_idx in trange(num):
